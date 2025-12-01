40a79206f5ea1fe82743261e5da8368204576746
xeureka <teklemariameureka@gmail.com>
2025-12-01 08:44:02 +0300

    day 1 of aoc_2025

--- 2025/Day-1/main.py
+++ 2025/Day-1/main.py
@@ -28,29 +28,10 @@ def solution(lines):
             count += 1
     return count
 
-def count_zero_hits(lines):
-    pos = 50
-    zeros = 0
-    for line in lines:
-        line = line.strip()
-        if not line:
-            continue
-        dir_char = line[0].upper()
-        steps = int(line[1:])
-        if dir_char == 'L':
-            pos = (pos - steps) % 100
-        elif dir_char == 'R':
-            pos = (pos + steps) % 100
-        else:
-            raise ValueError(f"Unknown direction: {dir_char}")
-        if pos == 0:
-            zeros += 1
-    return zeros
+
 
 with open("input.txt") as f:
     lines = f.readlines()
 
-answer = count_zero_hits(lines)
 ans = solution(lines)
 print(ans)
-print(answer)
